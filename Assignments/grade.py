3d06c535c3e6cc340bc48a697c68865e4ac1382c
Abirami k <abiramikanthasamy@gmail.com>
2026-08-13 01:38:08 +0530

    Update Python calculator

---
+++ Assignments/grade.py
@@ -0,0 +1,9 @@
+mark=int(input("enter the mark: "))
+if(mark>60):
+    print("the student got A grade")
+elif(mark>50):
+    print("The student got B grade")
+elif(mark>40):
+    print("The student got C grade")
+else:
+    print("The student failed in exam")
