1b69921d29879ce69f5837678b5e86e000c4318d
Abirami k <abiramikanthasamy@gmail.com>
2026-08-21 15:36:19 +0530

    upated

--- 11_DS_Phython.py
+++ 11_DS_Phython.py
@@ -2,6 +2,7 @@
 Data Structure:
 ---------------
     It is used to storing and organizing the data
+    
 
     Need:
     ->Store data
@@ -16,6 +17,49 @@ Data Structure:
 
 Data Structure in python:
 -------------------------
+-------------------------
+There ae 3 types of Datastructure:
+----------------------------------
+    Linear
+    non-linear
+    in-built
+
+Ds in python:
+    String
+    List 
+    Tuples
+    Set
+    Dictionary
+    Stack
+    Tree
+
+Phython supports: Single quotes, double quotes, Triple quotes also
+
+Time Complexity:
+----------------
+    how much time the algorithm takes to execute a input size
+Space Complexity:
+-----------------
+    how much memory needed by the algorithm takes to execute the input size
 
 
 '''
+number=(1,)
+print(type(number))
+numb=[1]
+print(type(numb))
+num={ }
+print(type(num))
+
+'''
+    list vs tuple
+Tuples is faster than list,because its immutabilty
+
+try towrite a name some basic infromation regarding your 
+'''
+A={
+    "name":"Abirami",
+    "email":"abiramikanthasamy@gmail.com",
+    "blood":"b+ve"
+}
+print(type(A))
