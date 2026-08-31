5e1b19dca08a84feb9c69a7763fa711c4ebe24ed
Abirami k <abiramikanthasamy@gmail.com>
2026-08-31 14:10:00 +0530

    Updated

--- Assignments/task_manager.py
+++ Assignments/task_manager.py
@@ -7,23 +7,24 @@ print("5. Exit task")
 
 choice=int(input("Enter the task: "))
 
-if( choice==1):
-    task=input("Enter a task: ")
-    tasks.append(task)
-    print("Task has been added")
-elif choice==2:
-    if(len(tasks)==0):
-        print("There is no task")
+while True:
+    if( choice==1):
+        task=input("Enter a task: ")
+        tasks.append(task)
+        print("Task has been added")
+    elif choice==2:
+        if(len(tasks)==0):
+            print("There is no task")
+        else:
+            print(tasks)
+    elif choice==3:
+        task=input("enter the task to remove: ")
+        if task in tasks:
+            tasks.remove(task)
+            print("Task is removed")
+        else:
+            print("Task not present")
+    elif choice==4:
+        print("No of task: ",len(tasks))
     else:
-        print(tasks)
-elif choice==3:
-    task=input("enter the task to remove: ")
-    if task in tasks:
-        tasks.remove(task)
-        print("Task is removed")
-    else:
-        print("Task not present")
-elif choice==4:
-    print("No of task: ",len(tasks))
-else:
-    exit(0)
+        exit(0)
