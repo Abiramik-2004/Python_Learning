3054e3ebec2e820ef1798e15645e7cd521e74089
Abirami k <abiramikanthasamy@gmail.com>
2026-09-07 14:24:59 +0530

    Updated

--- practice/stone_Paper_Scissor.py
+++ practice/stone_Paper_Scissor.py
@@ -2,36 +2,36 @@ import random
 flag=True
 comp,You,Draw=0,0,0
 while flag:
-    choices=["Rock","Paper","Scissors"]
+    choices=["rock","paper","scissors"]
     print("Welcome to Rock-Paper-Scissors game")
-    s=input("Do any one from Rock paper Scissors: ")
+    s=input("Do any one from Rock paper Scissors: ").lower()
     computer_choice=random.choice(choices)
     print("Computer Choice is ",computer_choice)
-    if s=="Rock":
-        if computer_choice=="Paper":
+    if s=="rock":
+        if computer_choice=="paper":
             print("You loss😓....best of luck")
             comp=comp+1
-        elif computer_choice=="Scissors":
+        elif computer_choice=="scissors":
             print("You win😎.....Congratulation!!!")
             You=You+1
         else:
             print("You and computer put the same sign......")
             Draw=Draw+1;
-    elif s=="Paper":
-        if computer_choice=="Scissors":
+    elif s=="paper":
+        if computer_choice=="scissors":
             print("You loss😓....best of luck")
             comp=comp+1
-        elif computer_choice=="Rock":
+        elif computer_choice=="rock":
             print("You win😎.....Congratulation!!!")
             You=You+1
         else:
             print("You and computer put the same sign......")
             Draw=Draw+1;
-    elif s=="Scissors":
-        if computer_choice=="Rock":
+    elif s=="scissors":
+        if computer_choice=="rock":
             print("You loss😓....best of luck")
             comp=comp+1
-        elif computer_choice=="Paper":
+        elif computer_choice=="paper":
             print("You win😎.....Congratulation!!!")
             You=You+1
         else:
