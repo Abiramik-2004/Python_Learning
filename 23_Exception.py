f80bb68dcf59ad0269410bc11e5e1f7907dfe336
Abirami k <abiramikanthasamy@gmail.com>
2026-09-21 16:36:05 +0530

    Updated

--- 23_Exception.py
+++ 23_Exception.py
@@ -5,3 +5,22 @@ except ValueError:
     print("Invalid Error")
 else:
     print(num)
+#--------------------------------------------------------
+try:
+    print(5/0)
+except Exception as e:
+    print("Expection occured")
+finally:
+    print("This block going to execute always...")
+
+#-----------------------------------------------------------
+
+age=int(input("Enter the age: "))
+if age<18:
+    try:
+        raise ValueError("Age must be eighteen or more...")
+    except:
+        print("Please enter the age more than 18...")
+
+else:
+    print("eligibe")
