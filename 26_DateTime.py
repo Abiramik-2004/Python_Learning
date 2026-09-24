bbf258feb7f343f5daa2666d78fcc637ab476a94
Abirami k <abiramikanthasamy@gmail.com>
2026-09-24 16:28:28 +0530

    Updated

--- 26_DateTime.py
+++ 26_DateTime.py
@@ -6,7 +6,15 @@ print(today)
 print("Year: ",today.year)
 print("month: ",today.month)
 print("day: ",today.day)
-
+#---------------------------------
 from datetime import datetime
 now=datetime.now()
 print (now)
+print("Year: ",now.year)
+print("month: ",now.month)
+print("day: ",now.day)
+print("hours: ",now.hour)
+print("minutes: ",now.minute)
+print("seconds: ",now.second)
+print("microseconds: ",now.microsecond)
+
